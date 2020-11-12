a8a46f4bc7d7809b280d19075e1916fdb456291f
Moritz Bugiel <mc.bugiel@googlemail.com>
2020-11-12 17:02:21 +0100

    basic prototype

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  console.log("Hallo")
+  initUpdateNavbarOnScroll();
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Ingredient.create(name: 'mint leaves')
+
+URL = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_all = open(URL).read
+ingredient = JSON.parse(ingredient_all)
+
+ingredient['drinks'].each do |i|
+  i.each do |_k, v|
+    Ingredient.create(name: v)
+  end
+end
